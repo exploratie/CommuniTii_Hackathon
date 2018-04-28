0aba3a2cabd1eee67aad390cccfbe34379385fa7
Abdal Alam <abdal720@gmail.com>
2018-04-28 14:12:11 -0400

    created messageList.js file
    - created messageList.js file under messaging folder

---
+++ apps/mobile/src/screens/messaging/messageList.js
@@ -0,0 +1,10 @@
+import React from "react"
+import { View, Text } from "react-native"
+
+const MessagingScreen = () => (
+  <View>
+    <Text>Message List</Text>
+  </View>
+)
+
+export default MessagingScreen
