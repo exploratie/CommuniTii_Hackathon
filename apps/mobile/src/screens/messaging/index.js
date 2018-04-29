64bac4cbdad88157077d855ec6dead201889bdcf
Ahmad Raza <raza2157@gmail.com>
2018-04-29 10:15:57 -0400

    Added chat log, picture

--- apps/mobile/src/screens/messaging/index.js
+++ apps/mobile/src/screens/messaging/index.js
@@ -1,10 +1,166 @@
-import React from "react"
-import { View, Text } from "react-native"
+import React, { Component } from "react"
+import {
+  Container,
+  Header,
+  Content,
+  List,
+  ListItem,
+  Left,
+  Body,
+  Right,
+  Thumbnail,
+  Text,
+  Item,
+  Icon,
+  Button,
+  Input
+} from "native-base"
 
-const MessagingScreen = () => (
-  <View>
-    <Text>This is Messaging</Text>
-  </View>
-)
-
-export default MessagingScreen
+export default class ListAvatarExample extends Component {
+  render() {
+    return (
+      <Container>
+        <Header searchBar rounded>
+          <Item>
+            <Icon name="ios-search" />
+            <Input placeholder="Search" />
+            <Icon name="ios-people" />
+          </Item>
+          <Button transparent>
+            <Text>Search</Text>
+          </Button>
+        </Header>{" "}
+        <Content>
+          <List>
+            <ListItem avatar>
+              <Left>
+                <Thumbnail source={require("../../../assets/kanye.jpg")} />
+              </Left>
+              <Body>
+                <Text>Kanye West</Text>
+                <Text note>
+                  Doing what you like will always keep you happy . .
+                </Text>
+              </Body>
+              <Right>
+                <Text note>3:43 pm</Text>
+              </Right>
+            </ListItem>
+            <ListItem avatar>
+              <Left>
+                <Thumbnail source={require("../../../assets/kanye.jpg")} />
+              </Left>
+              <Body>
+                <Text>Kanye West</Text>
+                <Text note>
+                  Doing what you like will always keep you happy . .
+                </Text>
+              </Body>
+              <Right>
+                <Text note>3:43 pm</Text>
+              </Right>
+            </ListItem>
+            <ListItem avatar>
+              <Left>
+                <Thumbnail source={require("../../../assets/kanye.jpg")} />
+              </Left>
+              <Body>
+                <Text>Kanye West</Text>
+                <Text note>
+                  Doing what you like will always keep you happy . .
+                </Text>
+              </Body>
+              <Right>
+                <Text note>3:43 pm</Text>
+              </Right>
+            </ListItem>
+            <ListItem avatar>
+              <Left>
+                <Thumbnail source={require("../../../assets/kanye.jpg")} />
+              </Left>
+              <Body>
+                <Text>Kanye West</Text>
+                <Text note>
+                  Doing what you like will always keep you happy . .
+                </Text>
+              </Body>
+              <Right>
+                <Text note>3:43 pm</Text>
+              </Right>
+            </ListItem>
+            <ListItem avatar>
+              <Left>
+                <Thumbnail source={require("../../../assets/kanye.jpg")} />
+              </Left>
+              <Body>
+                <Text>Kanye West</Text>
+                <Text note>
+                  Doing what you like will always keep you happy . .
+                </Text>
+              </Body>
+              <Right>
+                <Text note>3:43 pm</Text>
+              </Right>
+            </ListItem>
+            <ListItem avatar>
+              <Left>
+                <Thumbnail source={require("../../../assets/kanye.jpg")} />
+              </Left>
+              <Body>
+                <Text>Kanye West</Text>
+                <Text note>
+                  Doing what you like will always keep you happy . .
+                </Text>
+              </Body>
+              <Right>
+                <Text note>3:43 pm</Text>
+              </Right>
+            </ListItem>
+            <ListItem avatar>
+              <Left>
+                <Thumbnail source={require("../../../assets/kanye.jpg")} />
+              </Left>
+              <Body>
+                <Text>Kanye West</Text>
+                <Text note>
+                  Doing what you like will always keep you happy . .
+                </Text>
+              </Body>
+              <Right>
+                <Text note>3:43 pm</Text>
+              </Right>
+            </ListItem>
+            <ListItem avatar>
+              <Left>
+                <Thumbnail source={require("../../../assets/kanye.jpg")} />
+              </Left>
+              <Body>
+                <Text>Kanye West</Text>
+                <Text note>
+                  Doing what you like will always keep you happy . .
+                </Text>
+              </Body>
+              <Right>
+                <Text note>3:43 pm</Text>
+              </Right>
+            </ListItem>
+            <ListItem avatar>
+              <Left>
+                <Thumbnail source={require("../../../assets/kanye.jpg")} />
+              </Left>
+              <Body>
+                <Text>Kanye West</Text>
+                <Text note>
+                  Doing what you like will always keep you happy . .
+                </Text>
+              </Body>
+              <Right>
+                <Text note>3:43 pm</Text>
+              </Right>
+            </ListItem>
+          </List>
+        </Content>
+      </Container>
+    )
+  }
+}
